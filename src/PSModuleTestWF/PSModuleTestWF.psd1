371becf043d64c90cbc646abeb2b4d69a43fe236
Marius Storhaug <marstor@hotmail.com>
2024-02-21 10:47:20 +0100

    Fix tests

---
+++ src/PSModuleTestWF/PSModuleTestWF.psd1
@@ -0,0 +1,5 @@
+@{
+    ModuleVersion = '0.0.1'
+    RootModule    = 'PSModuleTestWF.psm1'
+    Description   = 'PSModule Framework Test Module'
+}
---
+++ src/PSModuleTestWF/public/Test-PSModuleTest.ps1
@@ -0,0 +1,18 @@
+﻿Function Test-PSModuleTest {
+    <#
+        .SYNOPSIS
+        Performs tests on a module.
+
+        .EXAMPLE
+        Test-PSModuleTest -Name 'World'
+
+        "Hello, World!"
+    #>
+    [CmdletBinding()]
+    param (
+        # Name of the person to greet.
+        [Parameter(Mandatory)]
+        [string] $Name
+    )
+    Write-Output "Hello, $Name!"
+}
